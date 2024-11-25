2fcc87ec3f546310a7e1dd50d640240fb58556b2
BenTalagan <ben_talagan@github.nospam.com>
2024-11-25 21:42:23 +0100

    Testing

---
+++ View/talagan_Docking tools.lua
@@ -0,0 +1,21 @@
+--[[
+@description Docking tools : actions to resize docks
+@version 0.1
+@author Ben 'Talagan' Babut
+@license MIT
+@metapackage
+@provides
+  [main=main]             talagan_Docking tools/actions/talagan_Maximize bottommost dock.lua                              > talagan_Maximize bottommost dock.lua
+  [main=main]             talagan_Docking tools/actions/talagan_Minimize bottommost dock.lua                              > talagan_Minimize bottommost dock.lua
+  [main=main]             talagan_Docking tools/actions/talagan_Set bottommost dock height (500).lua                      > talagan_Set bottommost dock height (500).lua
+  [main=main,midi_editor] talagan_Docking tools/actions/talagan_Maximize dock containing active MIDI Editor.lua           > talagan_Maximize dock containing active MIDI Editor.lua
+  [main=main,midi_editor] talagan_Docking tools/actions/talagan_Minimize dock containing active MIDI Editor.lua           > talagan_Minimize dock containing active MIDI Editor.lua
+  [main=main,midi_editor] talagan_Docking tools/actions/talagan_Set dock containing active MIDI Editor height (500).lua   > talagan_Set dock containing active MIDI Editor height (500).lua
+  [nomain] talagan_Docking tools/docking_lib.lua
+@changelog
+  - Initial Release
+@about
+  This package provide actions to quickly resize the MIDI dock and the bottommost dock.
+
+  You can copy / paste the "... (500).lua" action files and modify their name to put a custom height instead of 500.
+--]]
